e04467ade110c96a0786df69e6f03696edf92c6e
iamdevopstrainer <sahilkumar2275@gmail.com>
2023-05-28 11:13:11 +0530

    Dom mainpulation

--- script.js
+++ script.js
@@ -1,14 +1,1 @@
-
-function display() {
-  var form = document.getElementById("myform")
-  var name = form.elements["name"].value
-  var email = form.elements["email"].value
-  var phone = form.elements["phone"].value
-  var datetime = form.elements["datetime"].value
-
-  console.log("Name: " + name)
-  console.log("Email: " + email)
-  console.log("Phone: " + phone)
-  console.log("Datetime: " + datetime)
-}
- console.log("i made a change")
+// console.log(document.getElementById('items'))
